124fb5fdd2c542bb5918b101f35215ca3dff370d
urbanex <urbanex89@gmail.com>
2017-11-13 22:30:00 +0100

    modul 15.3 zadanie podejscie 1

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,13 +2,17 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
-    Task findById(@Param("ID") long id);
+    @Override
+    Optional<Task> findById(Long id);
+
+    @Override
+    Task save(Task task);
 }
